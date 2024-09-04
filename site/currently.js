5143464a2630f683a13b2a15dab814db7603d7ba
Soren Astra <eudoxe@protonmail.com>
2024-09-04 14:37:57 +0200

    mostly the currently box

---
+++ site/currently.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("currentplay").innerHTML = "nothing";
+	document.getElementById("currentlisten").innerHTML = "Kraftwerk";
+	document.getElementById("currentwork").innerHTML = "Learning the piano, designing a robotsona";
+});
