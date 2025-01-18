5edec9ddcaa4bb55209dfc1bbe762159fdbdf3e7
Soren Astra <eudoxe@protonmail.com>
2025-01-18 21:58:19 +0100

    projects and iframes

--- site/currently.js
+++ site/currently.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-	document.getElementById("currentplay").innerHTML = "Pokemon Emerald Seaglass";
-	document.getElementById("currentlisten").innerHTML = "Kraftwerk";
-	document.getElementById("currentwork").innerHTML = "Learning the piano, designing a robotsona";
+	document.getElementById("currentplay").innerHTML = "FFXIV?";
+	document.getElementById("currentlisten").innerHTML = "my own thoughts for once";
+	document.getElementById("currentwork").innerHTML = "Soren 3D model, task tracker game";
+	document.getElementById("currentthink").innerHTML = "Mad that I can't customize the scrollbars in Firefox";
 });
