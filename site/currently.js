dfb00cee094b916cb0d386dd7371709110f7a9a9
Soren Astra <eudoxe@protonmail.com>
2025-02-02 15:13:12 +0100

    Blog update

--- site/currently.js
+++ site/currently.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-	document.getElementById("currentplay").innerHTML = "FFXIV?";
-	document.getElementById("currentlisten").innerHTML = "my own thoughts for once";
+	document.getElementById("currentplay").innerHTML = "Amber Isle";
+	document.getElementById("currentlisten").innerHTML = "Not Spotify anymore, yay!";
 	document.getElementById("currentwork").innerHTML = "Soren 3D model, task tracker game";
-	document.getElementById("currentthink").innerHTML = "Mad that I can't customize the scrollbars in Firefox";
+	document.getElementById("currentthink").innerHTML = "Blender geometry nodes are too powerful for me";
 });
